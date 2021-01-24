b5beb58c7981c98f58f0538f20c8e41205d31d20
Geoff Matis <gmatis@gmail.com>
2021-01-24 15:33:50 -0500

    saving app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import numpy 
+import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask
+from flask import Flask, jsonify
 
 # #################################################
 # # Database Setup
@@ -50,11 +50,69 @@ def prcp():
     
     session.close()
 
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+    all_prcp = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        all_prcp.append(prcp_dict)
 
-    return jsonify(all_passengers)
+    return jsonify(all_prcp)
+
+#Tobs
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    results = session.query(Station.station, Station.name).order_by(Station.station).all()
+    
+    session.close()
+
+    all_stations = list(np.ravel(results))
+   
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+
+    results = session.query(Measurement.date, Measurement.tobs).\
+    filter(Measurement.date >= '2016-08-23').filter(Measurement.date <= '2017-08-23').\
+    filter(Measurement.station == 'USC00519281').all()
+    
+    session.close()
+
+    all_tobs = []
+    for date, tobs in results:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        all_tobs.append(tobs_dict)
+   
+    return jsonify(all_tobs)
+
+@app.route("/api/v1.0/start_date")
+def start(start_date):
+    session = Session(engine)
+
+    results = session.query(func.min(Measurement.tobs), 
+              func.max(Measurement.tobs),
+              func.avg(Measurement.tobs)).filter(Measurement.date >= start_date).all()
+    
+    session.close()
+
+    all_start_dates = []
+    for min_tobs, max_tobs, avg_tobs in results:
+        mma_tobs_dict = {}
+        mma_tobs_dict["min tobs"] = min_tobs
+        mma_tobs_dict["max tobs"] = max_tobs
+        mma_tobs_dict["avg tobs"] = avg_tobs
+
+        all_start_dates.append(mma_tobs_dict)
+   
+    return jsonify(all_start_dates)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
